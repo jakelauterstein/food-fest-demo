4d7c29882c052a4d64908c276c86fc7bfd12147c
Jake Lauterstein <97852239+jakelauterstein@users.noreply.github.com>
2022-06-15 12:14:37 -0500

    added offline functionality

--- webpack.config.js
+++ webpack.config.js
@@ -46,5 +46,13 @@ module.exports = {
         analyzerMode: "static", // the report outputs to an HTML file in the dist folder
       })
     ],
+    devServer: {
+      static: {
+          directory: path.join(__dirname, './'),
+      },
+      compress: true,
+      port: 8080,
+  },
+
       mode: 'development'
 };
